da7b01c7867584bab40c093fd5c225ffd7ed31b8
Darren Lam <darren.lam2@ucalgary.ca>
2025-10-27 14:10:37 -0600

    Initial Vite and TailwindCSS setup + partially setup themes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = () => {
+    return (
+        <div className="min-h-screen ">
+
+            {/* Theme Toggle */}
+
+            {/* BG Effects */}
+
+            {/* Nav Bar */}
+
+            {/* Main Content */}
+
+
+            {/* Footer */}
+        </div>   
+    );
+};
