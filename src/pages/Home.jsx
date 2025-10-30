371c05f2890fd402cad8f4650463de1401a6d533
drn4lm <darren4lam@gmail.com>
2025-10-29 18:56:37 -0600

    Finished hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "../components/Navbar";
-
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
     return (
         <div className="min-h-screen  bg-background text-foreground overflow-x-hidden">
 
             {/* Theme Toggle */}
-                <ThemeToggle />
+            <ThemeToggle />
             {/* BG Effects */}
-                <StarBackground />
+            <StarBackground />
             {/* Nav Bar */}
-                <Navbar />
+            <Navbar />
             {/* Main Content */}
-
+            <main>
+                <HeroSection />
+            </main>
 
             {/* Footer */}
         </div>   
