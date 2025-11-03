7c0883a1cc6b4b384a87667142624f454f1b965e
Darren Lam <darren.lam2@ucalgary.ca>
2025-11-03 14:07:03 -0700

    Finished about me section and started skills section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -21,7 +21,7 @@ export const HeroSection = () => {
           </h1>
 
           <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
-            I specialize in designing and developing software that balances performance, clarity, and impact. 
+            I specialize in designing and developing software that balances performance and clarity.
             What is my goal? It is to create technology that not only works—but works beautifully.
           </p>
 
@@ -34,8 +34,7 @@ export const HeroSection = () => {
       </div>
 
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
-        <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
-        <ArrowDown className="h-5 w-5 text-primary" />
+        <ArrowDown className="h-25 w-5 text-secondary" />
       </div>
     </section>
   );
